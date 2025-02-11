d7660e450e561f998659a9f43ac6ffba692230fe
gitPrintln <ghdcks101@naver.com>
2025-02-11 22:29:32 +0900

    1. 국내/해외 여행 체크 후 검색하면 검색창을 찾아서 원하는 검색값이랑 같으면 바로 클릭해서 확인 & 원하는 검색값이 아니라면 그냥 거기서 머물기 2. 검색창 Enter키 가능

--- travelPJ/app.py
+++ travelPJ/app.py
@@ -3,6 +3,7 @@ from flask import Flask, send_from_directory, render_template
 from flask import request, redirect, jsonify
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 app = Flask(__name__, template_folder='resources/templates', static_folder='resources/static')
 
@@ -20,22 +21,68 @@ def favicon():
 @app.route('/search', methods=['post'])
 def mainSearch():
     data = request.get_json()
+    selection = data.get('selection')
     query = data.get('query')
     print("query 값:", query)  # 터미널에 출력
     print("query 타입:", type(query))  # 타입 확인
+    
+    # 검색 데이터가 있으면
     if query:
-        search_url = f"https://search.naver.com/search.naver?query={query} 여행"
-        naverSearch(search_url)  # 검색 결과로 리다이렉트
+        search_url = "https://travel.naver.com/domestic"
+        naverSearch(search_url, selection, query)  # 검색 결과로 리다이렉트
         return jsonify({"message": "success"})
     else:
         return jsonify({"message": "fail"})
     
 # 검색 과정(selenium, webdriver)
-def naverSearch(search_url):
+def naverSearch(search_url, selection, query):
+    
     # 네이버 여행지 사이트로 이동
     browser = webdriver.Chrome() # 크롬창 띄우기
     browser.maximize_window() # 창 최대화
     browser.get(search_url) # 네이버 여행지
+    time.sleep(1)  # 페이지 로딩 대기
+    
+    from selenium.webdriver.common.action_chains import ActionChains
+    # 클래스명으로 검색 버튼 요소 찾기(같은게 두 개 있으므로 두번째꺼를 찾기위해 XPATH 사용)
+    searchButton = browser.find_element(By.XPATH, "(//a[@class='header_search__4UCHI'])[2]")
+    searchButton.click()
+    
+    time.sleep(1)  # 페이지 로딩 대기
+    
+    # 국내 여행/해외 여행 클릭
+    if selection == 'overseas':
+        locationSelecetBtn = browser.find_element(By.XPATH, "(//a[@class='searchbox_home_tab__RNL7F'])[2]")
+        locationSelecetBtn.click()
+        
+    
+    # 검색창 요소 찾아서 입력 후 리스트 출력
+    searchInput = browser.find_element(By.CLASS_NAME, "searchInput_input__oXdGi")
+    searchInput.send_keys(query)
+    
+    time.sleep(1)  # 페이지 로딩 대기
+    
+    # 리스트 요소들을 찾고 span이 query랑 같은 값이면 클릭
+    listSpanElements = browser.find_elements(By.CLASS_NAME, "searchbox_home_subject__8vLI5")
+    
+    # 첫 번째 값이 찾는 값과 완전 같을 경우 클릭
+    firstText = listSpanElements[0].text.strip()
+    if firstText == query:
+        listSpanElements[0].click()
+    """     
+    # 그게 아니라면 유사한 값이 있다는 것이기 때문에    
+    for spanElement in listSpanElements[1:]:
+        text = spanElement.text.strip()
+        if text == query:
+            spanElement.click()
+            break """
+            
+
+    
+    
+    
+    
+    
     
 
  
--- travelPJ/resources/static/js/main.js
+++ travelPJ/resources/static/js/main.js
@@ -1,20 +1,20 @@
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    
+    enter();
 });
 // 검색창 검색 버튼 클릭했을 때
 function searchSubmit() {
+    selection = document.getElementById("selected").value;
     query = document.getElementById("searchInput").value;
     fetch('/search', { method: 'post',
         headers: {
             'Content-Type': 'application/json'  // JSON 형식
         },
-        body: JSON.stringify({ query: query })
+        body: JSON.stringify({ selection: selection, query: query })
      })  // Flask에 요청 보내기
         .then(response => response.json())
         .then(data => {
-            console.log(data)
             if(data.message === 'success'){
                 // 검색창 초기화
                 clearInput();
@@ -24,10 +24,23 @@ function searchSubmit() {
         }).catch(error => {
             // 에러가 발생한 경우 catch 블록에서 처리
             console.error('Error:', error);
+            clearInput(); // 검색 결과가 없을 때도 초기화
         });
 }
 
 // 검색 완료 후 검색창 초기화
 function clearInput() {
     document.getElementById("searchInput").value = '';  // 빈 값으로 변경
+}
+
+// Enter로 동작 가능하게 하기
+function enter() {
+    searchInput = document.getElementById("searchInput");
+    submitBtn = document.getElementById("submitBtn");
+    searchInput.addEventListener('keydown', function(event) {
+        // 엔터 키(키 코드 13)가 눌렸을 때
+        if (event.key === 'Enter') {
+            submitBtn.click();
+        }
+    });
 }
