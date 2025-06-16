9852911efc5dba88d43898ecefdfe551b7a04478
gitPrintln <ghdcks101@naver.com>
2025-06-16 22:45:33 +0900

    postgresql 접속 잘되는지 테스트완료

--- HandLog/backend/app/database.py
+++ HandLog/backend/app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -9,6 +9,7 @@ DATABASE_URL = "postgresql://postgres:1234@localhost:5432/postgres"
 # SQLAlchemy 엔진 생성 > PostgreSQL DB에 연결
 engine = create_engine(
             DATABASE_URL,
+            connect_args={"client_encoding": "utf8"},
             pool_size=10,  # 풀 크기
             max_overflow=20,  # 풀을 초과한 커넥션 수
             pool_timeout=30,  # 커넥션을 얻기 위한 최대 대기 시간
